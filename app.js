0108a044693e159c9e9286abf1f65d5a81a3bbe9
github username <github email address>
2019-11-17 13:00:24 -0600

    added switch statements

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function testSize(num) {
 
 
 
-  
+
   var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 function golfScore(par, strokes) {
   // Only change code below this line
@@ -52,3 +52,30 @@ if(strokes == 1){
 
 // Change these values to test
 golfScore(5, 4);
+
+
+
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+  switch(val){
+    case 1:
+      answer = "alpha";
+    break;
+    case 2:
+     answer = "beta";
+    break;
+    case 3:
+     answer = "gamma";
+    break;
+    case 4:
+     answer = "delta";
+    break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  
+  // Change this value to test
+  caseInSwitch(1);
+  
